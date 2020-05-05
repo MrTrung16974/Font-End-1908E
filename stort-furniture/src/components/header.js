972b2c02b456d129e9208a154553f1cb86b7b279
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-05-05 23:20:10 +0700

    update

---
+++ stort-furniture/src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HelloWords</h1>
+            </div>
+        );
+    }
+}
+
+export default header;
