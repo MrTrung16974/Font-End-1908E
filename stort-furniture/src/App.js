b6f60c82aa12a4cc30d7d40d672ee75d538e58d2
Your Name <you@example.com>
2020-05-06 15:10:55 +0700

    update

--- stort-furniture/src/App.js
+++ stort-furniture/src/App.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import Header from './components/header.js'
+import Footer from './components/footer.js'
+import Content from './components/main/content.js'
+import Slide from './components/main/slide.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          {/* start Header */}
             <Header />
+          {/* End Header */}
+          {/* start Main */}
+            <Slide />
+          {/* End Main */}
       </div>
     );
   }
